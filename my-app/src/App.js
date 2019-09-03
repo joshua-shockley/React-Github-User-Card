db3bc8aed3a3906a73e2b1a8761186abe938d851
Joshua-Shockley <thelumbashockley@gmail.com>
2019-09-03 14:19:21 -0500

    saving starting point

---
+++ my-app/src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    };
+  }
+
+
+  render(){
+  return (
+    <div className="App">
+    <p>hello</p>
+    </div>
+  );
+  }
+}
+
+export default App;
